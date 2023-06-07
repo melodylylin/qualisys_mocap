9f2e38e717d2c3a2501ea664c6beefbc6731bb0e
ZP Yang <yang2zhanpeng@gmail.com>
2023-06-07 16:33:38 -0400

    added 3d marker streaming and tf publisher

--- src/qualisys_node.cpp
+++ src/qualisys_node.cpp
@@ -4,8 +4,11 @@
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
 #include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "geometry_msgs/msg/point.hpp"
+#include "visualization_msgs/msg/marker.hpp"
+#include "tf2_ros/transform_broadcaster.h"
 
 #include "qualisys_cpp_sdk/RTProtocol.h"
 #include "qualisys_cpp_sdk/RTPacket.h"
@@ -16,142 +19,253 @@
 
 using namespace std::chrono_literals;
 
-
 class QualisysNode : public rclcpp::Node
 {
-  private:
-    typedef rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr posePubPtr;
-    std::map<std::string, posePubPtr> pub_pose_{};
-    CRTProtocol rtProtocol_;
-    std::string serverAddr_{"127.0.0.1"};
-    const unsigned short basePort_{22222};
-    const int majorVersion_{1};
-    const int minorVersion_{19};
-    const bool bigEndian_{false};
-    unsigned short udpPort_{6734};
+private:
+	typedef rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr posePubPtr;
+	typedef rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr markerPubPtr;
+	typedef std::vector<geometry_msgs::msg::Point> Markers;
+	std::map<std::string, posePubPtr> pub_pose_{};
+	std::map<std::string, markerPubPtr> pub_marker_{};
+	std::vector<std::string> rb_names_{};
+	CRTProtocol rtProtocol_;
+	std::string serverAddr_{"127.0.0.1"};
+	const unsigned short basePort_{22222};
+	const int majorVersion_{1};
+	const int minorVersion_{22};
+	const bool bigEndian_{false};
+	unsigned short udpPort_{6734};
+	std::string frameComponents_{"3D 6D"};
+	std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
-  public:
-    QualisysNode()
-    : Node("qualisys_node") {
-      serverAddr_ = "192.168.123.2";
-      run();
-    }
+public:
+	QualisysNode()
+		: Node("qualisys_node")
+	{
+		declare_parameter("server", "192.168.123.2");
+		get_parameter("server", serverAddr_);
+		// serverAddr_ = "100.117.200.199";
+		tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+		run();
+	}
 
-    ~QualisysNode() {
-      rtProtocol_.StopCapture();
-      rtProtocol_.Disconnect();
-    }
+	~QualisysNode()
+	{
+		rtProtocol_.StopCapture();
+		rtProtocol_.Disconnect();
+	}
 
-  private:
-    void run() {
-        bool dataAvailable{false};
-        bool streamFrames{false};
+private:
+	void run()
+	{
+		bool dataAvailable{false};
+		bool streamFrames{false};
 
-        while (rclcpp::ok()) {
-          if (!rtProtocol_.Connected())
-          {
-              if (!rtProtocol_.Connect(serverAddr_.c_str(), basePort_, &udpPort_, majorVersion_, minorVersion_, bigEndian_))
-              {
-                  printf("rtProtocol_.Connect: %s\n\n", rtProtocol_.GetErrorString());
-                  sleep(1);
-                  continue;
-              }
-          }
+		while (rclcpp::ok())
+		{
+			if (!rtProtocol_.Connected())
+			{
+				if (!rtProtocol_.Connect(serverAddr_.c_str(), basePort_, &udpPort_, majorVersion_, minorVersion_, bigEndian_))
+				{
+					printf("rtProtocol_.Connect: %s\n\n", rtProtocol_.GetErrorString());
+					sleep(1);
+					continue;
+				}
+			}
 
-          if (!dataAvailable)
-          {
-              if (!rtProtocol_.Read6DOFSettings(dataAvailable))
-              {
-                  printf("rtProtocol_.Read6DOFSettings: %s\n\n", rtProtocol_.GetErrorString());
-                  sleep(1);
-                  continue;
-              }
-          }
+			if (!dataAvailable)
+			{
+				if (!rtProtocol_.Read6DOFSettings(dataAvailable))
+				{
+					printf("rtProtocol_.Read6DOFSettings: %s\n\n", rtProtocol_.GetErrorString());
+					sleep(1);
+					continue;
+				}
 
-          if (!streamFrames)
-          {
-              if (!rtProtocol_.StreamFrames(CRTProtocol::RateAllFrames, 0, udpPort_, NULL, CRTProtocol::cComponent6d))
-              {
-                  printf("rtProtocol_.StreamFrames: %s\n\n", rtProtocol_.GetErrorString());
-                  sleep(1);
-                  continue;
-              }
-              streamFrames = true;
+				if (!rtProtocol_.Read3DSettings(dataAvailable))
+				{
+					printf("rtProtocol_.Read3DSettings: %s\n\n", rtProtocol_.GetErrorString());
+					sleep(1);
+					continue;
+				}
+			}
 
-              printf("Starting to streaming 6DOF data\n\n");
-          }
+			if (!streamFrames)
+			{
+				// if (!rtProtocol_.StreamFrames(CRTProtocol::RateAllFrames, 0, udpPort_, NULL, CRTProtocol::cComponent6d))
+				if (!rtProtocol_.StreamFrames(CRTProtocol::RateAllFrames, 0, udpPort_, NULL, frameComponents_.c_str()))
+				{
+					printf("rtProtocol_.StreamFrames: %s\n\n", rtProtocol_.GetErrorString());
+					sleep(1);
+					continue;
+				}
+				streamFrames = true;
 
-          CRTPacket::EPacketType packetType;
+				printf("Starting to streaming mocap 3D/6DOF data\n\n");
+			}
 
-          if (rtProtocol_.Receive(packetType, true) == CNetwork::ResponseType::success)
-          {
-              if (packetType == CRTPacket::PacketData)
-              {
-                  float fX, fY, fZ;
-                  float rotationMatrix[9];
+			CRTPacket::EPacketType packetType;
 
-                  CRTPacket* rtPacket = rtProtocol_.GetRTPacket();
+			if (rtProtocol_.Receive(packetType, true) == CNetwork::ResponseType::success)
+			{
+				if (packetType == CRTPacket::PacketData)
+				{
+					float fX, fY, fZ;
+					float rotationMatrix[9];
 
-                  printf("Frame %d\n", rtPacket->GetFrameNumber());
-                  printf("======================================================================================================================\n");
+					CRTPacket *rtPacket = rtProtocol_.GetRTPacket();
 
-                  for (unsigned int i = 0; i < rtPacket->Get6DOFBodyCount(); i++)
-                  {
-                      if (rtPacket->Get6DOFBody(i, fX, fY, fZ, rotationMatrix))
-                      {
-                          const char* pTmpStr = rtProtocol_.Get6DOFBodyName(i);
-                          if (pTmpStr)
-                          {
-                              printf("%-12s ", pTmpStr);
-                          }
-                          else
-                          {
-                              printf("Unknown     ");
-                          }
-                          printf("Pos: %9.3f %9.3f %9.3f    Rot: %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n",
-                              fX, fY, fZ, rotationMatrix[0], rotationMatrix[1], rotationMatrix[2],
-                              rotationMatrix[3], rotationMatrix[4], rotationMatrix[5], rotationMatrix[6], rotationMatrix[7], rotationMatrix[8]);
+					// printf("Frame %d\n", rtPacket->GetFrameNumber());
+					// printf("======================================================================================================================\n");
+					// printf("rbs: %d\nmarkers: %d\n", rtPacket->Get6DOFBodyCount(), rtPacket->Get3DMarkerCount());
 
-                          if (pTmpStr) {
-                            if (pub_pose_.find(pTmpStr) == pub_pose_.end()) {
-                              pub_pose_[pTmpStr] = this->create_publisher<geometry_msgs::msg::PoseStamped>(pTmpStr, 10);
-                            }
+					for (unsigned int i = 0; i < rtPacket->Get6DOFBodyCount(); i++)
+					{
+						if (rtPacket->Get6DOFBody(i, fX, fY, fZ, rotationMatrix))
+						{
+							const char *pTmpStr = rtProtocol_.Get6DOFBodyName(i);
+							if (pTmpStr)
+							{
+								rb_names_.push_back(pTmpStr);
+								// printf("%-12s ", pTmpStr);
+							}
+							else
+							{
+								printf("Unknown     ");
+							}
+							// printf("Pos: %9.3f %9.3f %9.3f    Rot: %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f %6.3f\n",
+							// 	   fX, fY, fZ, rotationMatrix[0], rotationMatrix[1], rotationMatrix[2],
+							// 	   rotationMatrix[3], rotationMatrix[4], rotationMatrix[5], rotationMatrix[6], rotationMatrix[7], rotationMatrix[8]);
 
-			    // convert to quaternion
-			    tf2::Matrix3x3 R(
-				rotationMatrix[0],rotationMatrix[3],rotationMatrix[6],
-			        rotationMatrix[1],rotationMatrix[4],rotationMatrix[7],
-			        rotationMatrix[2],rotationMatrix[5],rotationMatrix[8]);
-			    tf2::Quaternion q;
-			    R.getRotation(q);
+							if (pTmpStr && !std::isnan(fX))
+							{
+								if (pub_pose_.find(pTmpStr) == pub_pose_.end())
+								{
+									pub_pose_[pTmpStr] = this->create_publisher<geometry_msgs::msg::PoseStamped>(std::string{pTmpStr}+"/pose", 10);
+								}
 
-                            // publish body
-                            geometry_msgs::msg::PoseStamped msg;
-			    msg.header.stamp = rclcpp::Clock().now();
-			    msg.header.frame_id = "qualisys";
-			    msg.pose.position.x = fX;
-			    msg.pose.position.y = fY;
-			    msg.pose.position.z = fZ;
-			    msg.pose.orientation.x = q.x();
-			    msg.pose.orientation.y = q.y();
-			    msg.pose.orientation.z = q.z();
-			    msg.pose.orientation.w = q.w();
-                            pub_pose_[pTmpStr]->publish(msg);
-                          }
-                      }
-                  }
-                  printf("\n");
-              }
-          }
-      }
-    }
+								// convert to quaternion
+								tf2::Matrix3x3 R(
+									rotationMatrix[0], rotationMatrix[3], rotationMatrix[6],
+									rotationMatrix[1], rotationMatrix[4], rotationMatrix[7],
+									rotationMatrix[2], rotationMatrix[5], rotationMatrix[8]);
+								tf2::Quaternion q;
+								R.getRotation(q);
+
+								// publish body
+								geometry_msgs::msg::PoseStamped msg;
+								msg.header.stamp = rclcpp::Clock().now();
+								msg.header.frame_id = "qualisys";
+								msg.pose.position.x = fX / 1000.0;
+								msg.pose.position.y = fY / 1000.0;
+								msg.pose.position.z = fZ / 1000.0;
+								msg.pose.orientation.x = q.x();
+								msg.pose.orientation.y = q.y();
+								msg.pose.orientation.z = q.z();
+								msg.pose.orientation.w = q.w();
+								pub_pose_[pTmpStr]->publish(msg);
+
+								// TF message
+								geometry_msgs::msg::TransformStamped tf_msg;
+
+								// Read message content and assign it to
+								// corresponding tf variables
+								tf_msg.header.stamp = rclcpp::Clock().now();
+								tf_msg.header.frame_id = "qualisys";
+								tf_msg.child_frame_id = pTmpStr;
+
+								tf_msg.transform.translation.x = fX / 1000.0;
+								tf_msg.transform.translation.y = fY / 1000.0;
+								tf_msg.transform.translation.z = fZ / 1000.0;						
+								tf_msg.transform.rotation.x = q.x();
+								tf_msg.transform.rotation.y = q.y();
+								tf_msg.transform.rotation.z = q.z();
+								tf_msg.transform.rotation.w = q.w();
+
+								// Send the transformation
+								tf_broadcaster_->sendTransform(tf_msg);
+							}
+						}
+					}
+					// printf("\n");
+					
+					std::map<std::string, Markers> rb_marker_map_;
+					for (unsigned int i = 0 ; i < rtPacket->Get3DMarkerCount(); i++)
+					{
+						if (rtPacket->Get3DMarker(i, fX, fY, fZ))
+						{
+							std::string rb_name;
+							// int marker_idx;
+							std::vector<std::string> substr_list;
+							std::string delimiter = " - ";
+							const char *pTmpStr = rtProtocol_.Get3DLabelName(i);
+							if (pTmpStr && !std::isnan(fX))
+							{
+								// printf("%-12s ", pTmpStr);
+								std::string str{pTmpStr};
+
+								int start = 0;
+								int end = str.find(delimiter);
+								
+								while (end != -1)
+								{
+									substr_list.push_back(std::string(str.substr(0, end)));
+									start = end + delimiter.size();
+									end = str.find(delimiter, start);
+								}
+								substr_list.push_back(std::string(str.substr(start, end)));
+
+								rb_name = substr_list.front();
+								// marker_idx = (int)substr_list.back();
+
+								if (rb_marker_map_.find(rb_name) == rb_marker_map_.end())
+								{
+									rb_marker_map_[rb_name] = Markers{};
+								}
+								
+								// std::cout << "filling markers...\n";
+								geometry_msgs::msg::Point point;
+								point.x = fX / 1000.0;
+								point.y = fY / 1000.0;
+								point.z = fZ / 1000.0;
+								rb_marker_map_[rb_name].push_back(point);
+							}
+							else
+							{
+								printf("Unknown    ");
+							}
+							// printf("x: %9.3f y: %9.3f z: %9.3f\n", fX, fY, fZ);
+						}
+					}
+					for (auto i = rb_marker_map_.begin(); i != rb_marker_map_.end(); i++)
+					{
+						if (pub_marker_.find(i->first) == pub_marker_.end())
+						{
+							pub_marker_[i->first] = this->create_publisher<visualization_msgs::msg::Marker>(i->first+"/markers", 10);
+							// std::cout << "Marker pub created: " << i->first << "\n";
+						}
+						visualization_msgs::msg::Marker marker_msg;
+						marker_msg.header.stamp = rclcpp::Clock().now();
+						marker_msg.header.frame_id = "qualisys";
+						marker_msg.type = visualization_msgs::msg::Marker::POINTS;
+						marker_msg.points = i->second;
+						marker_msg.scale.x = .03;
+						marker_msg.scale.y = .03;
+						marker_msg.color.g = 1.0;
+						marker_msg.color.a = 1.0;
+						marker_msg.action = visualization_msgs::msg::Marker::ADD;
+						pub_marker_[i->first]->publish(marker_msg);
+					}
+				}
+			}
+		}
+	}
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-  rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<QualisysNode>());
-  rclcpp::shutdown();
-  return 0;
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<QualisysNode>());
+	rclcpp::shutdown();
+	return 0;
 }
-
